fd0bcd9667f18209c75091cc4cf9e3c71ce37be5
sy-records <52o@qq52o.cn>
2022-03-27 11:01:15 +0800

    Fix typo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const footnotes = function(hook) {
   hook.beforeEach(function (html) {
-    if (/\[\^\d+\][^:]/.test(html)) {
+    if (/\[\^([A-Za-z0-9]+)\][^:]/.test(html)) {
       html = html
         .replace(/\[\^([A-Za-z0-9]+)\][^:]/gm, '<sup class="footnote-symbol" id="fn-$1">[\[$1]\](#fnref-$1)</sup>')
         .replace(/\[\^([A-Za-z0-9]+)\]\: /gm, '<strong class="footnote-reference-symbol" id="fnref-$1">[\[$1\]](#fn-$1)</strong>:leftwards_arrow_with_hook: ');
